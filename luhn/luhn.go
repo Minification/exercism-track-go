27aead75c71739547877a8db79c653e17a5af9df
Marius Hubatschek <marius.hubatschek@gmail.com>
2021-09-01 07:56:49 +0200

    Revert Luhn back

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -2,14 +2,14 @@
 // as per the Luhn formula.
 package luhn
 
-/*var lookup = [2][10]int{
+var lookup = [2][10]int{
 	{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
 	{0, 2, 4, 6, 8, 1, 3, 5, 7, 9},
-}*/
+}
 
 // Valid checks whether the specified string represents a valid number according
 // to the Luhn formula.
-/*func Valid(number string) bool {
+func Valid(number string) bool {
 	var i, sum, digitCount, double int
 	for i = len(number) - 1; i >= 0; i-- {
 		digit := number[i]
@@ -26,29 +26,4 @@ package luhn
 		}
 	}
 	return sum%10 == 0 && digitCount > 1
-}*/
-
-// Valid determines whether the digits in the given string constitute a valid luhn code
-func Valid(s string) bool {
-	var n, d, i, m int
-	for i = len(s) - 1; i >= 0; i-- {
-		c := s[i]
-		switch {
-		case c == ' ':
-			continue
-		case c >= '0' && c <= '9':
-			m = int(c - '0')
-			if d%2 == 1 {
-				m <<= 1
-			}
-			if m > 9 {
-				m -= 9
-			}
-			n += m
-			d++
-		default:
-			return false
-		}
-	}
-	return d > 1 && n%10 == 0
 }
